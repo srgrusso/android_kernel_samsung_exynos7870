2aeedc582d8caa5a6bd63a9c0649be6e36829b03
Sultanxda <sultanxda@gmail.com>
2016-10-21 01:37:16 -0700

    proc: Remove verifiedbootstate flag from /proc/cmdline
    Userspace parses this and sets the ro.boot.verifiedbootstate prop
    according to the value that this flag has. When ro.boot.verifiedbootstate
    is not 'green', SafetyNet is tripped and fails the CTS test.
    
    Hide verifiedbootstate from /proc/cmdline in order to fix the failed
    SafetyNet CTS check.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>
    Signed-off-by: Michele Beccalossi <beccalossi.michele@gmail.com>

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -2,6 +2,9 @@
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
+#include <asm/setup.h>
+
+static char new_command_line[COMMAND_LINE_SIZE];
 
 #ifdef CONFIG_SECURITY_SELINUX_PERMISSIVE
 #include <asm/setup.h>
@@ -13,8 +16,7 @@ static int cmdline_proc_show(struct seq_file *m, void *v)
 {
 #ifdef CONFIG_SECURITY_SELINUX_PERMISSIVE
 	seq_printf(m, "%s\n", proc_cmdline);
-#else
-	seq_printf(m, "%s\n", saved_command_line);
+	seq_printf(m, "%s\n", new_command_line);
 #endif
 	return 0;
 }
@@ -34,6 +36,30 @@ static const struct file_operations cmdline_proc_fops = {
 static int __init proc_cmdline_init(void)
 {
 #ifdef CONFIG_SECURITY_SELINUX_PERMISSIVE
+
+   char *offset_addr, *cmd = new_command_line;
+
+	strcpy(cmd, saved_command_line);
+
+	/*
+	 * Remove 'androidboot.verifiedbootstate' flag from command line seen
+	 * by userspace in order to pass SafetyNet CTS check.
+	 */
+	offset_addr = strstr(cmd, "androidboot.verifiedbootstate=");
+	if (offset_addr) {
+		size_t i, len, offset;
+
+		len = strlen(cmd);
+		offset = offset_addr - cmd;
+
+		for (i = 1; i < (len - offset); i++) {
+			if (cmd[offset + i] == ' ')
+				break;
+		}
+
+		memmove(offset_addr, &cmd[offset + i + 1], len - i - offset);
+	}
+
 	char *a1, *a2;
 
 	a1 = strstr(saved_command_line, "androidboot.selinux=");
