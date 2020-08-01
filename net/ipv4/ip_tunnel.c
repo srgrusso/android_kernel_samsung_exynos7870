49e9b252b4f229394db7faed78fbd43a5a981690
TALU <taluemail272@gmail.com>
2020-08-01 23:57:53 +0100

    ip_gre: revert "Add support to collect tunnel metadata." and related commits.
    Commit dad6f799877899b2504d8b82b67b0c688006fb70 "ip_gre: Add
    support to collect tunnel metadata." added an include for
    net/dst_metadata.h in net/ipv4/ip_gre.c, which doesn't exist,
    so compilation fails when CONFIG_NET_IPGRE is enabled. Just
    revert that commit and all commits related to it.
    
    This reverts commits dad6f799877899b2504d8b82b67b0c688006fb70
    "ip_gre: Add support to collect tunnel metadata.",
    7e1986db30fd97494e7b91aaa29ef2a50cc6389f "gre: do not keep the
    GRE header around in collect medata mode",
    78cf93efb90c323509d6b6916cbce0062b6aaf4b "net, ip_tunnel: fix
    namespaces move", 39be947c35ca1a5d38e6eb9809b466636d0581ee
    "gre: do not assign header_ops in collect metadata mode" and
    59204adbae89ae5eb5ef0c0362055a5fc6777a67 "gre: reject GUE and
    and FOU in collect metadata mode".

--- net/ipv4/ip_tunnel.c
+++ net/ipv4/ip_tunnel.c
@@ -228,10 +228,6 @@ struct ip_tunnel *ip_tunnel_lookup(struct ip_tunnel_net *itn,
 	if (cand)
 		return cand;
 
-	t = rcu_dereference(itn->collect_md_tun);
-	if (t)
-		return t;
-
 	ndev = READ_ONCE(itn->fb_tunnel_dev);
 	if (ndev && ndev->flags & IFF_UP)
 		return netdev_priv(ndev);
@@ -263,15 +259,11 @@ static void ip_tunnel_add(struct ip_tunnel_net *itn, struct ip_tunnel *t)
 {
 	struct hlist_head *head = ip_bucket(itn, &t->parms);
 
-	if (t->collect_md)
-		rcu_assign_pointer(itn->collect_md_tun, t);
 	hlist_add_head_rcu(&t->hash_node, head);
 }
 
-static void ip_tunnel_del(struct ip_tunnel_net *itn, struct ip_tunnel *t)
+static void ip_tunnel_del(struct ip_tunnel *t)
 {
-	if (t->collect_md)
-		rcu_assign_pointer(itn->collect_md_tun, NULL);
 	hlist_del_init_rcu(&t->hash_node);
 }
 
@@ -427,8 +419,7 @@ static struct ip_tunnel *ip_tunnel_create(struct net *net,
 }
 
 int ip_tunnel_rcv(struct ip_tunnel *tunnel, struct sk_buff *skb,
-		  const struct tnl_ptk_info *tpi, struct metadata_dst *tun_dst,
-		  bool log_ecn_error)
+		  const struct tnl_ptk_info *tpi, bool log_ecn_error)
 {
 	struct pcpu_sw_netstats *tstats;
 	const struct iphdr *iph = ip_hdr(skb);
@@ -486,9 +477,6 @@ int ip_tunnel_rcv(struct ip_tunnel *tunnel, struct sk_buff *skb,
 	} else {
 		skb->dev = tunnel->dev;
 	}
-
-	if (tun_dst)
-		skb_dst_set(skb, (struct dst_entry *)tun_dst);
 
 	gro_cells_receive(&tunnel->gro_cells, skb);
 	return 0;
@@ -827,7 +815,7 @@ static void ip_tunnel_update(struct ip_tunnel_net *itn,
 			     struct ip_tunnel_parm *p,
 			     bool set_mtu)
 {
-	ip_tunnel_del(itn, t);
+	ip_tunnel_del(t);
 	t->parms.iph.saddr = p->iph.saddr;
 	t->parms.iph.daddr = p->iph.daddr;
 	t->parms.i_key = p->i_key;
@@ -988,7 +976,7 @@ void ip_tunnel_dellink(struct net_device *dev, struct list_head *head)
 	itn = net_generic(tunnel->net, tunnel->ip_tnl_net_id);
 
 	if (itn->fb_tunnel_dev != dev) {
-		ip_tunnel_del(itn, netdev_priv(dev));
+		ip_tunnel_del(netdev_priv(dev));
 		unregister_netdevice_queue(dev, head);
 	}
 }
@@ -1077,13 +1065,8 @@ int ip_tunnel_newlink(struct net_device *dev, struct nlattr *tb[],
 	nt = netdev_priv(dev);
 	itn = net_generic(net, nt->ip_tnl_net_id);
 
-	if (nt->collect_md) {
-		if (rtnl_dereference(itn->collect_md_tun))
-			return -EEXIST;
-	} else {
-		if (ip_tunnel_find(itn, p, dev->type))
-			return -EEXIST;
-	}
+	if (ip_tunnel_find(itn, p, dev->type))
+		return -EEXIST;
 
 	nt->net = net;
 	nt->parms = *p;
@@ -1099,6 +1082,7 @@ int ip_tunnel_newlink(struct net_device *dev, struct nlattr *tb[],
 		dev->mtu = mtu;
 
 	ip_tunnel_add(itn, nt);
+
 out:
 	return err;
 }
@@ -1172,8 +1156,6 @@ int ip_tunnel_init(struct net_device *dev)
 	iph->version		= 4;
 	iph->ihl		= 5;
 
-	if (tunnel->collect_md)
-		netif_keep_dst(dev);
 	return 0;
 }
 EXPORT_SYMBOL_GPL(ip_tunnel_init);
@@ -1185,7 +1167,7 @@ void ip_tunnel_uninit(struct net_device *dev)
 	struct ip_tunnel_net *itn;
 
 	itn = net_generic(net, tunnel->ip_tnl_net_id);
-	ip_tunnel_del(itn, netdev_priv(dev));
+	ip_tunnel_del(netdev_priv(dev));
 	if (itn->fb_tunnel_dev == dev)
 		WRITE_ONCE(itn->fb_tunnel_dev, NULL);
 
